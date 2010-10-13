1df4456f0ca9dddbe3932d81d1242b61ce93a361
Mike Lin <mlin@mlin.net>
2010-10-12 22:14:31 -0400

    initial import

---
+++ Sqlite3EZ.ml
@@ -0,0 +1,172 @@
+open List
+open Sqlite3
+
+module Rc = Sqlite3.Rc
+module Data = Sqlite3.Data
+
+exception Rc of Rc.t
+exception Finally of exn*exn
+
+let check_rc = function
+	| Rc.OK | Rc.DONE -> ()
+	| rc -> raise (Rc rc)
+let bindall stmt a = Array.iteri (fun i x -> check_rc (bind stmt (i+1) x)) a
+
+type statement_instance = {
+	stmt : stmt;
+	parameter_count : int;
+	mutable in_progress : bool;
+}
+
+type statement = {
+	lazy_inst : statement_instance Lazy.t;
+	mutable forced : bool;
+	mutable finalized : bool;
+}
+
+let instance st =
+	if st.finalized then failwith "Sqlite3EZ: attempt to use finalized statement"
+	st.forced <- true
+	Lazy.force st.lazy_inst
+
+let make_statement' db sql =
+	let inst =
+		lazy
+			let stmt = prepare db sql
+			{stmt = stmt; parameter_count = bind_parameter_count stmt; in_progress = false}
+	let x = { lazy_inst = inst; forced = false; finalized = false }
+	Gc.finalise (function { forced = true; finalized = false; lazy_inst } -> ignore (finalize (Lazy.force lazy_inst).stmt) | _ -> ()) x
+	x
+
+let finally_reset statement f =
+	(* TODO handle expired/recompile *)
+	let instance = instance statement
+	if instance.in_progress then failwith "Sqlite3EZ: attempt to execute statement already in progress"
+	instance.in_progress <- true
+	try
+		let y = f instance
+		instance.in_progress <- false
+		check_rc (reset instance.stmt)
+		y
+	with
+		| exn ->
+			instance.in_progress <- false
+			try check_rc (reset instance.stmt) with exn2 -> raise (Finally (exn,exn2))
+			raise exn
+			
+
+let statement_exec statement parameters =
+	finally_reset statement
+		fun instance ->
+			if Array.length parameters <> instance.parameter_count then
+				invalid_arg "Sqlite3EZ.statement_exec: wrong number of parameters"
+			bindall instance.stmt parameters
+			let rc = ref Rc.OK
+			while !rc <> Rc.DONE do
+				rc := step instance.stmt
+				if !rc = Rc.ROW then failwith "Sqlite3EZ.statement_exec: not a (_ -> unit) statement"
+				check_rc !rc
+
+let statement_query statement parameters cons fold init =
+	finally_reset statement
+		fun instance ->
+			if Array.length parameters <> instance.parameter_count then
+				invalid_arg "Sqlite3EZ.statement_query : wrong number of parameters"
+			bindall instance.stmt parameters
+
+			let arbox = ref None
+			let x = ref init
+			let rc = ref (step instance.stmt)
+			while !rc = Rc.ROW do
+				let k = data_count instance.stmt
+				let ar = match !arbox with
+					| Some ar when Array.length ar = k -> ar
+					| Some _ -> failwith "Sqlite3EZ.statement_query: varying number of result columns"
+					| None ->
+						let ar = Array.make k Data.NULL
+						arbox := Some ar
+						ar
+				for i = 0 to k - 1 do
+					ar.(i) <- column instance.stmt i
+				x := fold (cons ar) !x
+				rc := step instance.stmt
+			check_rc !rc
+			!x
+			
+let statement_finalize_now x =
+	if x.forced && not x.finalized then
+		let inst = instance x
+		if inst.in_progress then failwith "Sqlite3EZ: attempt to finalize in-progress statement"
+		x.finalized <- true
+		ignore (finalize inst.stmt)
+	else
+		x.finalized <- true
+
+type database = {
+	h : db;
+	mutable still_open : bool;
+	
+	statement_begin : statement;
+	statement_commit : statement;
+	statement_rollback : statement;
+
+	statement_savepoint : statement;
+	statement_release : statement;
+	statement_rollback_to : statement;
+}
+
+let db_open fn = 
+	let h = db_open fn
+	let x =
+		{ h = h;
+			still_open = true;
+			statement_begin = make_statement' h "BEGIN";
+			statement_commit = make_statement' h "COMMIT";
+			statement_rollback = make_statement' h "ROLLBACK";
+			statement_savepoint = make_statement' h "SAVEPOINT ?";
+			statement_release = make_statement' h "RELEASE ?";
+			statement_rollback_to = make_statement' h "ROLLBACK TO ?";
+			 } 
+	Gc.finalise (function { still_open = true; h } -> ignore (db_close h) | _ -> ()) x
+	x
+
+let db_close_now = function
+	| x when x.still_open = true -> 
+		x.still_open <- false
+		ignore (db_close x.h)
+	| _ -> ()
+
+let db_handle { h } = h
+
+let exec { h } sql = check_rc (exec h sql)
+
+let empty = [||]
+let transact db f x =
+	statement_exec db.statement_begin empty
+	try
+		let y = f x
+		statement_exec db.statement_commit empty
+		y
+	with
+		| exn ->
+			try statement_exec db.statement_rollback empty with exn' -> raise (Finally (exn,exn'))
+			raise exn
+
+let p = [| Data.TEXT "A" |]
+let atomically db f x =
+	statement_exec db.statement_savepoint p
+	try
+		let y = f x
+		statement_exec db.statement_release p
+		y
+	with
+		| exn ->
+			try
+				statement_exec db.statement_rollback_to p
+				statement_exec db.statement_release p
+			with exn' -> raise (Finally (exn,exn'))
+			raise exn
+
+let last_insert_rowid db = last_insert_rowid db.h
+
+let make_statement { h } sql = make_statement' h sql
---
+++ Sqlite3EZ.mli
@@ -0,0 +1,58 @@
+(** simplified wrapper for [Sqlite3] *)
+
+(** as Sqlite3.Rc *)
+module Rc : sig
+	include module type of Sqlite3.Rc
+
+(** as Sqlite3.Data *)
+module Data : sig
+	include module type of Sqlite3.Data
+
+(** raised when an SQLite operation returns an unsuccessful return code *)
+exception Rc of Rc.t
+
+(** raised when we are trying to handle an exception and another exception is raised (e.g. we are executing a transaction, encounter an exception, and issue a ROLLBACK command, which itself fails) *)
+exception Finally of exn*exn
+
+(** a database handle *)
+type database
+
+(** as [Sqlite3.db_open] *)
+val db_open : string -> database
+
+(** immediately close the database. The database connection will otherwise be closed when garbage-collected. *)
+val db_close_now : database -> unit
+
+(** [transact db f x] evaluates [f x] within a BEGIN..COMMIT transaction. If [f x] evaluates successfully to [y], the transaction is committed and [y] is returned. If the evaluation of [f x] raises an exception, the transaction is rolled back and the exception is re-raised.
+
+Note that BEGIN..COMMIT transactions cannot be nested in SQLite. Any attempt to make a nested call to [transact] will raise an exception. *)
+val transact : database -> ('a -> 'b) -> 'a -> 'b
+
+(** [atomically db f x] evaluates [f x] within a SAVEPOINT..RELEASE transaction, which may be nested.
+
+This EZ implementation allows only parenthetically nested transactions, so there is no need to name savepoints. *)
+val atomically : database -> ('a -> 'b) -> 'a -> 'b
+
+(** execute some imperative SQL statement(s). Multiple statements may be separated by a semicolon. *)
+val exec : database -> string -> unit
+
+(** as [Sqlite3.last_insert_rowid *)
+val last_insert_rowid : database -> Int64.t
+
+(** a compiled statement, can be either imperative or a query *)
+type statement
+
+(** prepare a statement from the SQL text. The given string must contain only one SQL statement. The statement can be used multiple times (calls to [Sqlite3.reset] are handled automatically). The statement is not actually compiled until its first use. *)
+val make_statement : database -> string -> statement
+
+(** bind the given parameters and execute an imperative statement. An exception is raised if the statement attempts to return result rows. *)
+val statement_exec : statement -> Data.t array -> unit
+
+(** [statement_query stmt params cons fold init] binds the given parameters and executes a query. Each result row is first passed to [cons], which will usually construct a value from the data. This value is then passed to [fold] along with an intermediate value, which is [init] for the first row. This can used to build a list or other data structure from all the results. *)
+val statement_query : statement -> Data.t array -> (Data.t array -> 'a) -> ('a -> 'b -> 'b) -> 'b -> 'b
+
+(** immediately finalize the statement, releasing any associated resources. The statement will otherwise be finalized when garbage-collected. *)
+val statement_finalize_now : statement -> unit
+
+(**/**)
+val db_handle : database -> Sqlite3.db
